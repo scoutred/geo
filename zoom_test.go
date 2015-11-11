dd6a3d1e500e5efba06435f7060e55f71ca1ea2d
Alexander Rolek <a.rolek@gmail.com>
2015-11-10 18:45:16 -0800

    updated tests for boundsZoom

--- zoom_test.go
+++ zoom_test.go
@@ -31,26 +31,23 @@ func TestCetnterBoundsZoom(t *testing.T) {
 
 func TestBoundsZoom(t *testing.T) {
 	bounds := geo.NewLatLngBounds(
-		-117.166048186108, 32.7199686756585,
-		-117.16572277941, 32.7201589855535,
+		32.7204706651118, -117.16439634561537,
+		32.71965828903011, -117.1673735976219,
 	)
 
-	size := geometry.Point{
-		X: 1107,
-		Y: 360,
-	}
+	size := geometry.NewPoint(1107, 360)
 
 	proj := crs.NewEspg3857()
 
 	//	first test should max out at 30
 	zoom := BoundsZoom(proj, bounds, size, 30.0)
-	if zoom != 22.0 {
+	if zoom != 18.0 {
 		t.Error("boundZoom failed to zoom correctly")
 	}
 
 	//	make sure the max zoom works
-	zoom2 := BoundsZoom(proj, bounds, size, 19.0)
-	if zoom2 != 19.0 {
+	zoom = BoundsZoom(proj, bounds, size, 16.0)
+	if zoom != 16.0 {
 		t.Error("boundZoom failed to zoom correctly")
 	}
 }
